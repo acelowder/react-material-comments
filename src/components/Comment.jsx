11b4a87f5f2609a269353cf4f79331a84c027a3d
Ace <ace.lowder@gmail.com>
2024-05-17 16:59:00 -0700

    Add elapsed time in comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,5 @@
 import { Typography } from '@mui/material';
+import moment from 'moment';
 import './Comment.css';
 
 export default function Comment({ comment }) {
@@ -6,9 +7,21 @@ export default function Comment({ comment }) {
 		<div className="comment">
 			<img className="avatar" src={comment.avatar} />
 			<div>
-				<Typography variant="body2" className="username">
-					@<span>{comment.author}</span>
-				</Typography>
+				<div className="comment-head">
+					<Typography variant="body2" className="username">
+						@<span>{comment.author}</span>
+					</Typography>
+					<Typography
+						variant="body2"
+						sx={{
+							color: 'text.secondary',
+							fontSize: 12,
+							pb: 0.1,
+						}}
+					>
+						{moment(comment.created).fromNow()}
+					</Typography>
+				</div>
 				<Typography variant="body1">{comment.text}</Typography>
 			</div>
 		</div>
