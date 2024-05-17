354f32887ffbfd49abd376bedaf2c347748f01ab
Ace <ace.lowder@gmail.com>
2024-05-17 16:13:55 -0700

    Add profile pictures

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,16 @@
 import { Typography } from '@mui/material';
+import './Comment.css';
 
 export default function Comment({ comment }) {
 	return (
-		<div>
-			<Typography variant="body2">{comment.author}</Typography>
-			<Typography variant="body2" gutterBottom={true}>
-				{comment.text}
-			</Typography>
+		<div className="comment">
+			<img className="avatar" src={comment.avatar} />
+			<div>
+				<Typography variant="body2">@{comment.author}</Typography>
+				<Typography variant="body2" gutterBottom={true}>
+					{comment.text}
+				</Typography>
+			</div>
 		</div>
 	);
 }
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -7,6 +7,7 @@ const initialComments = [
 	{
 		id: uuidv4(),
 		author: 'Jane',
+		avatar: 'https://mui.com/static/images/avatar/3.jpg',
 		text: 'This is a comment.',
 		created: new Date('May 16, 2024 04:24:12'),
 		likes: 3,
@@ -15,6 +16,7 @@ const initialComments = [
 			{
 				id: uuidv4(),
 				author: 'John',
+				avatar: 'https://mui.com/static/images/avatar/1.jpg',
 				text: 'This is a reply.',
 				created: new Date('May 16, 2024 04:24:57'),
 				likes: 1,
@@ -25,6 +27,7 @@ const initialComments = [
 	{
 		id: uuidv4(),
 		author: 'John',
+		avatar: 'https://mui.com/static/images/avatar/1.jpg',
 		text: 'This is another comment.',
 		created: new Date('May 16, 2024 04:24:38'),
 		likes: 1,
