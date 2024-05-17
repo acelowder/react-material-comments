7ec4a18ede64a02892ec6e9565c7141dd347c3c3
Ace <ace.lowder@gmail.com>
2024-05-17 15:20:28 -0700

    Add replies

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,21 @@
 import { Typography } from '@mui/material';
+import './Comment.css';
 
 export default function Comment({ comment }) {
-	console.log(comment.created);
-
 	return (
 		<>
-			<Typography variant="body2">{comment.author}</Typography>
-			<Typography variant="body2" gutterBottom={true}>
-				{comment.text}
-			</Typography>
+			<div className="comment">
+				<Typography variant="body2">{comment.author}</Typography>
+				<Typography variant="body2" gutterBottom={true}>
+					{comment.text}
+				</Typography>
+			</div>
+			{comment.replies &&
+				comment.replies.map((reply) => (
+					<div className="reply">
+						<Comment key={reply.id} comment={reply} />
+					</div>
+				))}
 		</>
 	);
 }
