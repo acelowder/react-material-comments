f47d3085a59eb6cd6cb1ea3dbfae5cd68f9b2392
Ace <ace.lowder@gmail.com>
2024-05-20 17:34:55 -0700

    Add comment input form visuals

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,12 +1,16 @@
 import { useCommentsService } from './CommentSection.service.js';
+import CommentTitle from './CommentTitle';
+import CommentInput from './CommentInput';
 import Thread from './Thread';
-import './CommentSection.css';
 
 export default function CommentSection() {
-	const { comments } = useCommentsService();
+	const { comments, profile } = useCommentsService();
 
 	return (
-		<div className="comment-section">
+		<div style={{ width: '700px', padding: '20px' }}>
+			<CommentTitle />
+			<hr style={{ border: 'none', borderTop: '1px solid #888' }} />
+			<CommentInput />
 			{comments.map((comment) => (
 				<Thread key={comment.id} comment={comment} />
 			))}
---
+++ src/components/CommentTitle.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+
+export default function CommentTitle() {
+	return (
+		<Typography
+			variant="h4"
+			component="h1"
+			sx={{ mb: 1, fontWeight: 'bold' }}
+		>
+			💬 Leave a Comment
+		</Typography>
+	);
+}
